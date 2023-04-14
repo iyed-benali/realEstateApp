49cc9af8bd1db105594fee2458466bfc7356d8cd
iyed-benali <aveyrokepler86@gmail.com>
2023-04-14 18:21:40 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains:['wp-tid.zillowstatic.com',
+    "media.istockphoto.com"]
+  }
 }
 
 module.exports = nextConfig
